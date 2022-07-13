43fbd50da5799707c31d86da0f0e08ec5c85ad9a
Homayoun Mohseni <mohseni.homayoun@gmail.com>
2022-07-13 15:26:06 +0430

    implemet winner evaluation, minor tweaks

--- 1.py
+++ 1.py
@@ -4,17 +4,22 @@ class Player:
 		uid += 1
 		self.uid = uid - 1
 		self.wins_cnt = 0
+	def increment_wins(self):
+		self.wins_cnt += 1
 
+#TODO make classes of Square and Line and store instances of them in field instead of simple refrences to playes
+#TODO extent Player class, let Player have a refrence to field so it could independently play his or her turn
 class Square:
 	pass
 
 class Field:
-	def __init__(self, rows, cols):
+	def __init__(self, rows, cols, players_cnt):
 		self.rows = rows
 		self.cols = cols
 		self.lines_hor = [[False] * cols] * (rows + 1)
 		self.lines_ver = [[False] * (cols + 1)] * rows
 		self.squares = [[False] * cols] * rows
+		self.players_cnt = players_cnt
 
 	def fill(self, hor_ver_bar, row, col, player):
 		if (hor_ver_bar):
@@ -36,7 +41,7 @@ class Field:
 					return False
 		return True
 	
-	def eval_new_squares(hor_ver_bar, row, col, player):
+	def eval_new_squares(self, hor_ver_bar, row, col, player):
 		if (hor_ver_bar):
 			if row > 0:
 				if count_square_sides((row - 1, col), (row, col + 1)) == 3:
@@ -51,24 +56,41 @@ class Field:
 			if col < cols:
 				if count_square_sides((row, col), (row + 1, col + 1)) == 3:
 					square[row][col] = player
-
-
-
-
 	
+	def evalue_winner(self):
+		squares_cnt = [0] * players_cnt
+		for i in range(rows):
+			for j in range(cols):
+				squares_cnt[int(squares[i][j].uid)] += 1
+		max_squares = -1		
+		winner_index = -1
+		for i in range(players_cnt):
+			if squares_cnt[i] > max_squares:
+				max_squares = squares_cnt[i]
+				argmax = i
+		return winner_index
+
+
 
 n = int(input()) #players count
 m, k = [int(x) for x in input().split()] #rows and columns
+players = [Player(x) for x in range(n)]
 while True:
 	field = Field(n, m)
-	players = [Player(x) for x in range(n)]
 	turn = 0
 	while not field.finished():
 		player = players[turn]
+		hor_ver_bar, row, col = input("player " + player.uit + "'s turn:").split()
+		hor_ver_bar = hor_ver_bar == "True"
+		row = int(row)
+		col = int(col)
+
 		#get input row col:
 		if field.fill(hor_ver_bar, row, col, player):
 			turn += 1
 			turn %= n
 		else:
 			print("already filled")
-	winner = field.determine_winner()
+
+	winner_index = field.evaluate_winner()
+	palyers[winner_index].increment_wins()
